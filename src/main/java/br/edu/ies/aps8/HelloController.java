eec9feee63abe005d7d8e7ca4766fbac7f16a0f9
victorrgr <victoracc2001@hotmail.com>
2024-05-28 21:04:29 -0300

    beginning of authentication

--- src/main/java/br/edu/ies/aps8/HelloController.java
+++ src/main/java/br/edu/ies/aps8/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SomethingController {
+public class HelloController {
 
     @GetMapping
     public String hello() {
---
+++ src/main/java/br/edu/ies/aps8/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ies.aps8.repository;
+
+import br.edu.ies.aps8.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
